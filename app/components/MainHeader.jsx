3d6811a51285a9d022ab39912c497613280fa448
AbubekerJ <abubeker.jemal03@gmail.com>
2024-07-18 17:21:00 +0100

    Fix env error

--- app/components/MainHeader.jsx
+++ app/components/MainHeader.jsx
@@ -26,7 +26,7 @@ export default function MainHeader() {
           <RiSettings3Line className="header-icon" />
           <TbGridDots className="header-icon" />
         </div>
-        <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md hover:brightness-105 hover:shadow-md transition-all ml-2">
+        <button className="bg-blue-500 text-white px-6 py-2 font-medium rounded-md  hover:shadow-md transition-all ml-2">
           Sign in
         </button>
       </div>
